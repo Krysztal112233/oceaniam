bce18040c5950b38c4e5558acfc92c2fd872b24b
Krysztal Huang <krysztal.huang@outlook.com>
2026-01-06 14:01:35 +0800

    feat(oceaniam-export): add more export from `oceaniam-vo`

---
+++ crates/oceaniam-export/src/vo.rs
@@ -0,0 +1,26 @@
+pub mod auth;
+
+#[derive(ts_rs::TS)]
+#[ts(export)]
+#[ts(as = "oceaniam_vo::auth::SignupRequest")]
+pub struct SignupRequest;
+
+#[derive(ts_rs::TS)]
+#[ts(export)]
+#[ts(as = "oceaniam_vo::auth::SignupResponse")]
+pub struct SignupResponse;
+
+#[derive(ts_rs::TS)]
+#[ts(export)]
+#[ts(as = "oceaniam_vo::auth::SigninRequest")]
+pub struct SigninRequest;
+
+#[derive(ts_rs::TS)]
+#[ts(export)]
+#[ts(as = "oceaniam_vo::auth::SigninResponse")]
+pub struct SigninResponse;
+
+#[derive(ts_rs::TS)]
+#[ts(export)]
+#[ts(as = "oceaniam_vo::auth::SignoutResponse")]
+pub struct SignoutResponse;
