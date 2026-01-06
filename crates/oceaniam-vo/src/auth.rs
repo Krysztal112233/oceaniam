62e5a58a509f3fd7dc22995692ed8bf9a47893b1
Krysztal Huang <krysztal.huang@outlook.com>
2026-01-06 14:00:24 +0800

    feat(oceaniam-vo): add validator to vo

--- crates/oceaniam-vo/src/auth.rs
+++ crates/oceaniam-vo/src/auth.rs
@@ -1,18 +1,32 @@
+use garde::Validate;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-#[derive(Debug, Deserialize, ToSchema, ts_rs::TS)]
+#[derive(Debug, Deserialize, Validate, ToSchema, ts_rs::TS)]
 #[serde(untagged)]
 pub enum SignupRequest {
     /// Login via email
-    Email { email: String, pwd: String },
+    Email {
+        #[garde(email)]
+        email: String,
+        #[garde(skip)]
+        pwd: String,
+    },
 
-    /// Login via phone
-    Phone { phone: String, pwd: String },
+    /// Login via phonenumber
+    Phone {
+        #[garde(phone_number)]
+        phone: String,
+
+        #[garde(skip)]
+        pwd: String,
+    },
 }
 
 #[derive(Debug, Deserialize, ToSchema, ts_rs::TS)]
-pub struct SignupResponse {}
+pub struct SignupResponse {
+    pub jwt: String,
+}
 
 pub type SigninRequest = SignupRequest;
 
