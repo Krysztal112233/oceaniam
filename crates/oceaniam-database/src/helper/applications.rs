332c624a212a17ca74b9477a779f77b334e1c770
Krysztal Huang <krysztal.huang@outlook.com>
2026-02-15 00:40:06 +0800

    feat(oceaniam-database): add `PagedSelect`

--- crates/oceaniam-database/src/helper/applications.rs
+++ crates/oceaniam-database/src/helper/applications.rs
@@ -1,9 +1,11 @@
-use oceaniam_common::error::Error;
-use sea_orm::{ActiveModelTrait, EntityTrait, IntoActiveModel};
+use oceaniam_common::{PageParam, PagedResponse, error::Error};
+use sea_orm::{
+    ActiveModelTrait, ColumnTrait, EntityTrait, IntoActiveModel, PaginatorTrait, QueryFilter,
+};
 use uuid::Uuid;
 
 use crate::{
-    helper::SafeTransactionConnectionTrait,
+    helper::{PagedSelect, SafeTransactionConnectionTrait},
     model::{self, prelude::Applications},
 };
 
@@ -29,6 +31,28 @@ pub trait ApplicationHelper {
         database: &impl SafeTransactionConnectionTrait,
     ) -> Result<bool, Error> {
         Ok(Applications::find_by_id(id).one(database).await?.is_some())
+    }
+
+    async fn get_applications(
+        database: &impl SafeTransactionConnectionTrait,
+        tenants_id: Uuid,
+        page: &PageParam,
+    ) -> Result<PagedResponse<model::applications::Model>, Error> {
+        use crate::model::applications::Column::*;
+
+        let paginator = Applications::find()
+            .filter(TenantsId.eq(tenants_id))
+            .paged(*page)
+            .paginate(database, page.per_page);
+
+        let items = paginator.fetch_page(0).await?;
+        let total = paginator.num_items().await? as usize;
+        let has_next = (page.as_offset() + items.len() as u64) < total as u64;
+
+        Ok(PagedResponse {
+            items,
+            page_info: oceaniam_common::PageInfo { has_next, total },
+        })
     }
 }
 
--- crates/oceaniam-database/src/helper/mod.rs
+++ crates/oceaniam-database/src/helper/mod.rs
@@ -1,4 +1,5 @@
-use sea_orm::{ConnectionTrait, TransactionTrait};
+use oceaniam_common::PageParam;
+use sea_orm::{ConnectionTrait, EntityTrait, QuerySelect as _, Select, TransactionTrait};
 
 pub mod applications;
 pub mod credentials;
@@ -12,4 +13,17 @@ pub trait SafeTransactionConnectionTrait: TransactionTrait + ConnectionTrait + S
 impl<T> SafeTransactionConnectionTrait for T where
     T: TransactionTrait + ConnectionTrait + Send + Sync
 {
+}
+
+pub trait PagedSelect {
+    fn paged(self, paged: PageParam) -> Self;
+}
+
+impl<T> PagedSelect for Select<T>
+where
+    T: EntityTrait,
+{
+    fn paged(self, page: PageParam) -> Self {
+        self.offset(page.as_offset()).limit(page.as_limit())
+    }
 }
