c273b10a9d4f5ec16f15bd8f46193bf4ae7245ac
Krysztal Huang <krysztal.huang@outlook.com>
2026-04-08 23:18:54 +0800

    fix(oceaniam): fix tests failed

--- backend/crates/migration/src/m20260408_040329_enable_pg_cron.rs
+++ backend/crates/migration/src/m20260408_040329_enable_pg_cron.rs
@@ -8,18 +8,19 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
             .get_connection()
-            .execute_unprepared("CREATE EXTENSION IF NOT EXISTS pg_cron")
+            .execute_unprepared(
+                r#"
+                SELECT pg_advisory_lock(1145141919810);
+                CREATE EXTENSION IF NOT EXISTS pg_cron;
+                SELECT pg_advisory_unlock(1145141919810);
+                "#,
+            )
             .await?;
 
         Ok(())
     }
 
-    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .get_connection()
-            .execute_unprepared("DROP EXTENSION pg_cron CASCADE")
-            .await?;
-
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
         Ok(())
     }
 }
