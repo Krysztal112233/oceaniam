7ea0d8961e1235ad01fce3f257880e7e0395eb17
Krysztal Huang <krysztal.huang@outlook.com>
2026-06-07 01:32:09 +0800

    refactor(oceaniam-keybox): deduplicate `Algorithm ↔ KeyAlg` conversions with macro

--- backend/crates/oceaniam-keybox/src/key_alg.rs
+++ backend/crates/oceaniam-keybox/src/key_alg.rs
@@ -1,58 +1,59 @@
 use jsonwebtoken::Algorithm;
-use oceaniam_database::model::sea_orm_active_enums::KeyAlg as InnerKeyAlg;
+use oceaniam_database::model::sea_orm_active_enums::KeyAlg as DatabaseKeyAlgorithm;
 use serde::{Deserialize, Serialize};
 
 use crate::error::Error;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 #[serde(transparent)]
-pub struct KeyAlg(InnerKeyAlg);
+pub struct KeyAlg(DatabaseKeyAlgorithm);
 
-impl TryFrom<Algorithm> for KeyAlg {
-    type Error = crate::error::Error;
+macro_rules! alg_conversions {
+    ($(($jwt:ident, $db:ident)),+ $(,)?) => {
+        impl TryFrom<Algorithm> for KeyAlg {
+            type Error = Error;
 
-    fn try_from(value: Algorithm) -> Result<Self, Self::Error> {
-        let inner = match value {
-            Algorithm::RS256 => InnerKeyAlg::Rs256,
-            Algorithm::RS384 => InnerKeyAlg::Rs384,
-            Algorithm::RS512 => InnerKeyAlg::Rs512,
-            Algorithm::PS256 => InnerKeyAlg::Ps256,
-            Algorithm::PS384 => InnerKeyAlg::Ps384,
-            Algorithm::PS512 => InnerKeyAlg::Ps512,
-            _ => return Err(Error::unimplemented_jwt_alogrithm(value)),
-        };
+            fn try_from(value: Algorithm) -> Result<Self, Self::Error> {
+                match value {
+                    $(Algorithm::$jwt => Ok(Self(DatabaseKeyAlgorithm::$db)),)+
+                    _ => Err(Error::unimplemented_jwt_alogrithm(value)),
+                }
+            }
+        }
 
-        Ok(KeyAlg(inner))
-    }
+        impl From<KeyAlg> for Algorithm {
+            fn from(value: KeyAlg) -> Self {
+                match value.0 {
+                    $(DatabaseKeyAlgorithm::$db => Algorithm::$jwt),+
+                }
+            }
+        }
+    };
 }
 
-impl From<InnerKeyAlg> for KeyAlg {
-    fn from(value: InnerKeyAlg) -> Self {
+alg_conversions!(
+    (RS256, Rs256),
+    (RS384, Rs384),
+    (RS512, Rs512),
+    (PS256, Ps256),
+    (PS384, Ps384),
+    (PS512, Ps512),
+);
+
+impl From<DatabaseKeyAlgorithm> for KeyAlg {
+    fn from(value: DatabaseKeyAlgorithm) -> Self {
         Self(value)
     }
 }
 
-impl From<KeyAlg> for InnerKeyAlg {
+impl From<KeyAlg> for DatabaseKeyAlgorithm {
     fn from(value: KeyAlg) -> Self {
         value.0
     }
 }
 
-impl From<KeyAlg> for Algorithm {
-    fn from(value: KeyAlg) -> Self {
-        match value.0 {
-            InnerKeyAlg::Rs256 => Algorithm::RS256,
-            InnerKeyAlg::Rs384 => Algorithm::RS384,
-            InnerKeyAlg::Rs512 => Algorithm::RS512,
-            InnerKeyAlg::Ps256 => Algorithm::PS256,
-            InnerKeyAlg::Ps384 => Algorithm::PS384,
-            InnerKeyAlg::Ps512 => Algorithm::PS512,
-        }
-    }
-}
-
 impl std::ops::Deref for KeyAlg {
-    type Target = InnerKeyAlg;
+    type Target = DatabaseKeyAlgorithm;
 
     fn deref(&self) -> &Self::Target {
         &self.0
